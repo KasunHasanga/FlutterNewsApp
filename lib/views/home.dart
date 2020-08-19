f3fd6866f1b2d53c9d570259528536e6cecc3147
Kasun Hasanga <kasunhasanga96@gmail.com>
2020-08-19 21:13:54 +0530

    Add data to category Model

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -30,3 +30,17 @@ class _HomeState extends State<Home> {
     );
   }
 }
+class CategoryTile extends StatelessWidget {
+  final imageUrl ,categoryName;
+  CategoryTile({this.imageUrl,this.categoryName});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Image.network(imageUrl,width: 120,height: 60,)
+        ],
+      ),
+    );
+  }
+}
