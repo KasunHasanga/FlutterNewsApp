4e5546863e821a3c670e78ff4481b80361adaaaa
Kasun Hasanga <kasunhasanga96@gmail.com>
2020-08-20 01:15:39 +0530

    Category mode working|| UI part is Done

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -40,6 +40,7 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           children: <Widget>[
             Container(
@@ -68,11 +69,34 @@ class CategoryTile extends StatelessWidget {
   CategoryTile({this.imageUrl,this.categoryName});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: <Widget>[
-          Image.network(imageUrl,width: 120,height: 60,fit: BoxFit.fitWidth,)
-        ],
+    return GestureDetector(
+      onTap: (){
+
+      },
+      child: Container(
+
+        margin: EdgeInsets.only(right: 16),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Image.network(imageUrl,width: 120,height: 60,fit: BoxFit.fitWidth,)),
+            Container(
+            width: 120,
+              alignment:Alignment.center,
+              height: 60,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                color: Colors.black38
+              ),
+              child: Text(categoryName,
+
+              style: TextStyle(color: Colors.white,
+              fontSize: 14),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
